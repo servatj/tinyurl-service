d8d8d4e83269a8bad6286e1c131a81f0979644bc
servatj <jservatlorca@gmail.com>
2024-02-04 10:41:28 +0400

    Add config package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { URLModule } from './url/url.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     URLModule,
     MongooseModule.forRoot('mongodb://localhost:27017/tinyurl'),
   ],
